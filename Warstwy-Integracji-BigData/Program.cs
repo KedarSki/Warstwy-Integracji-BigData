8ea64fdb15348782046b7e3e166c0a8395ac1138
Radoslaw Zamojski <radoslaw.zamojski@gmail.com>
2022-10-29 10:46:11 +0200

    initial commit

---
+++ Warstwy-Integracji-BigData/Program.cs
@@ -0,0 +1,16 @@
+﻿using CsvHelper;
+using FileHelpers;
+using Warstwy_Integracji_BigData;
+
+Names names = new();
+
+var fileHelperEngine = new FileHelperEngine<Names>();
+var records = fileHelperEngine.ReadFile(@"C:\Git\Warstwy-Integracji-BigData\Warstwy-Integracji-BigData\Data\ImionaNadawaneDzieciom.csv");
+
+
+foreach (var name in records)
+{
+    Console.WriteLine(name.Name);
+    Console.WriteLine(name.Quantity);
+    Console.WriteLine(name.Sex);
+}
