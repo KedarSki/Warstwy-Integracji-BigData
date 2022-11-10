a33db910a8de0a52cd65fc82cc9d535d8a3a02d5
Radoslaw Zamojski <radoslaw.zamojski@gmail.com>
2022-11-10 13:52:14 +0100

    Changes that allows to create additional file

--- Warstwy-Integracji-BigData/Program.cs
+++ Warstwy-Integracji-BigData/Program.cs
@@ -1,16 +1,38 @@
 ﻿using CsvHelper;
 using FileHelpers;
-using Warstwy_Integracji_BigData;
-
-Names names = new();
-
-var fileHelperEngine = new FileHelperEngine<Names>();
-var records = fileHelperEngine.ReadFile(@"C:\Git\Warstwy-Integracji-BigData\Warstwy-Integracji-BigData\Data\ImionaNadawaneDzieciom.csv");
 
 
-foreach (var name in records)
+string[] names = System.IO.File.ReadAllLines(@"C:\Git\Warstwy-Integracji-BigData\Warstwy-Integracji-BigData\Data\ImionaNadawaneDzieciom.csv");
+string[] surNamesM = System.IO.File.ReadAllLines(@"C:\Git\Warstwy-Integracji-BigData\Warstwy-Integracji-BigData\Data\NazwiskaMeskie.csv");
+string[] surNamesF = System.IO.File.ReadAllLines(@"C:\Git\Warstwy-Integracji-BigData\Warstwy-Integracji-BigData\Data\NazwiskaZenskie.csv");
+string fullNames = @"C:\Git\Warstwy-Integracji-BigData\Warstwy-Integracji-BigData\Data\Full-Names.csv";
+
+string person = String.Empty;
+Random random = new Random();
+string delimiter = ",";
+
+for(int i = 0; i < 40000000; i++)
 {
-    Console.WriteLine(name.Name);
-    Console.WriteLine(name.Quantity);
-    Console.WriteLine(name.Sex);
+    int indexName = random.Next(names.Length);
+    var nameTab = names[indexName].Split(",");
+    var name = nameTab[0];
+    var sex = nameTab[2];
+
+    if(sex == "M")
+    {
+        int indexSurnameM = random.Next(names.Length);
+        person = name + delimiter + surNamesM[indexSurnameM].Split(",")[0];
+    }
+
+    else
+    {
+        int indexSurnameF = random.Next(names.Length);
+        person = name + delimiter + surNamesF[indexSurnameF].Split(",")[0];
+    }
+    string appendText = i.ToString() + delimiter + person + Environment.NewLine;
+
+    File.AppendAllText(fullNames, appendText);
+
+    Console.WriteLine(appendText);
+
 }
